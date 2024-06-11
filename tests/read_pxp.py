0c1a924aaf9b83ccd39cda7340ebb1c46e97140d
Laura Gonzalez <sofiagofou@gmail.com>
2024-06-11 16:54:25 +0200

    minor change

--- tests/read_pxp.py
+++ tests/read_pxp.py
@@ -3,11 +3,14 @@ import matplotlib.pylab as plt
 import os
 
 from igor2.packed import load as loadpxp
-# data = loadpxp('C:/Users/laura.gonzalez/DATA/nm28May2024c0/nm28May2024c0_000.pxp')
+
+data = loadpxp('C:/Users/laura.gonzalez/DATA/nm28May2024c0/nm28May2024c0_000.pxp')
 
 files = os.listdir('C:/Users/laura.gonzalez/DATA')
-for f in files:
-    if 'nm' in f:
-        print('- ', f)
+for file in files:
+    if 'nm' in file:
+        print('- ', file)
 
-#print(data)
+
+data = loadpxp('C:/Users/laura.gonzalez/DATA/nm28May2024c0/nm28May2024c0_000.pxp')
+print('hey', data, 'hey')
