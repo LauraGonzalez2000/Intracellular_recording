80fed17e545264326ae9cc1acbcc54a3e94ddea0
Laura Gonzalez <sofiagofou@gmail.com>
2024-06-11 14:32:01 +0200

    tests folder added

---
+++ tests/read_pxp.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pylab as plt
+import os
+
+from igor2.packed import load as loadpxp
+# data = loadpxp('C:/Users/laura.gonzalez/DATA/nm28May2024c0/nm28May2024c0_000.pxp')
+
+files = os.listdir('C:/Users/laura.gonzalez/DATA')
+for f in files:
+    if 'nm' in f:
+        print('- ', f)
+
+#print(data)
