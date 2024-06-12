fccd64e8f102414b79e63aff09db843d66a1e10c
Laura Gonzalez <sofiagofou@gmail.com>
2024-06-12 11:50:50 +0200

    structural changes

--- src/ALL_files_analysis.py
+++ src/ALL_files_analysis.py
@@ -4,18 +4,6 @@ import os
 
 from igor2.packed import load as loadpxp
 
-#data = loadpxp('C:/Users/laura.gonzalez/DATA/nm28May2024c0/nm28May2024c0_000.pxp')
-
-#files = os.listdir('C:/Users/laura.gonzalez/DATA')
-#for file in files:
-#    if 'nm' in file:
-#        print('- ', file)
-
-
-#data = loadpxp('C:/Users/laura.gonzalez/DATA/nm28May2024c0/nm28May2024c0_000.pxp')
-#print('hey', data, 'hey')
-
-
 def find_nm_files(root_folder):
     nm_paths = []
     
@@ -54,14 +42,3 @@ for file in files:
 
     #analyse each file to plot
 
-
-'''
-print('C:/Users/laura.gonzalez/DATA/nm03Jun2024c0/nm03Jun2024c0_000.pxp')
-print(files[0])
-print(str(files[0]))
-
-data = loadpxp('C:/Users/laura.gonzalez/DATA/nm28May2024c0/nm28May2024c0_000.pxp')
-print(data)
-#data = loadpxp(str(files[0]))
-#print(data)
-'''
