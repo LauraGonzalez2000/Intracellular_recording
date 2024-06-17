d71311973c48f9f42a7db83c5d712e02743c788c
Laura Gonzalez <sofiagofou@gmail.com>
2024-06-17 18:15:03 +0200

    loading data and extracting basic information

--- src/ALL_files_analysis.py
+++ src/ALL_files_analysis.py
@@ -1,8 +1,10 @@
-import numpy as np
-import matplotlib.pylab as plt
+
 import os
 
+from PdfPage import PdfPage
+from trace_analysis import DataFile
 from igor2.packed import load as loadpxp
+import pprint
 
 def find_nm_files(root_folder):
     nm_paths = []
@@ -20,25 +22,75 @@ def find_nm_files(root_folder):
             normalized_path = os.path.normpath(file_path)
             forward_slash_path = normalized_path.replace("\\", "/")
             nm_paths.append(forward_slash_path)
-            print('-', file)
+            #print('-', file)
 
     return nm_paths
 
-def load_data(file):
-    try:
-        data = loadpxp(file)
-        print("data was loaded")
-    except:
-        print("data was not loaded")
-
-###### MAIN
+###### MAIN ######################################################
 
 files = find_nm_files('C:/Users/laura.gonzalez/DATA/RAW_DATA')
-for file in files:
-    data = load_data(file)
-    #analyse each file to create a pdf
 
+
+for file in files:
+    datafile = DataFile(file)
+
+    '''
+    fill_PDF(filename, debug=True)
+    '''
     #analyse each file to create excel
 
     #analyse each file to plot
 
+
+#### try with one file, then put this inside the loop:
+#datafile = DataFile('C:/Users/laura.gonzalez/DATA/RAW_DATA/nm12Jun2024c0/nm12Jun2024c0_000.pxp')
+
+
+
+
+
+
+
+
+
+'''
+#print(datafile.response)
+#pprint.pp(datafile)
+
+#data = File('C:/Users/laura.gonzalez/DATA/RAW_DATA/nm02May2024c0/nm02May2024c0_000.pxp')  #in loop put "file"
+#data.load_data()
+
+#data_test = loadpxp('C:/Users/laura.gonzalez/DATA/RAW_DATA/nm12Jun2024c0/nm12Jun2024c0_000.pxp')
+#print(data_test)
+#print(data_test[0])
+#print(len(data_test))   #len = 2 , first item? second item is a dict
+
+#print(data_test[1].items())
+
+'''
+'''
+recordings = data_test[1]['root'][b'RecordA0']
+for value in recordings:
+    print(value)
+
+
+print(len(recordings
+print(recordings)
+'''
+'''
+#data_test[1][b'SampleInterval']
+#print(data_test[1]['V_Flag'])
+#print(data_test[1])
+
+
+#recordings = get_recordings(data)
+
+
+#record_a0 = data_test[1]['root'][b'RecordA0'] #['wave']['wData']
+#print("RecordA0:", record_a0)
+
+# Iterating over all RecordA keys
+#record_keys = [key for key in data_test[1]['root'].keys() if key.startswith(b'RecordA')]
+#for record_key in record_keys:
+#    print(f"{record_key.decode('utf-8')}: {data['root'][record_key]}")
+'''
